9c4ce59ed031a731a8ed2360cd303ed9364734fe
LinnSilver <baralinn@gmail.com>
2023-04-20 09:53:47 +0000

    Uppdate

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -17,6 +17,8 @@ else:
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
 db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
